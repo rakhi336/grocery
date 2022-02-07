91595f74175e1f51537820ba34d7248c805f1d43
rakhi336 <arohinur336@gmail.com>
2022-02-06 21:08:32 -0800

    groceryPro

---
+++ script.js
@@ -0,0 +1,94 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+} 
+
+let shoppingcart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingcart.classList.toggle('active');
+    
+    searchForm.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+} 
+
+let loginform = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginform.classList.toggle('active');
+    
+    searchForm.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    navbar.classList.remove('active');
+
+} 
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    
+    searchForm.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+
+} 
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingcart.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+
+} 
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+  autoplay: {
+delay: 7500,
+disableOnInteraction: false,
+  },
+  centerSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+  
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+  autoplay: {
+delay: 7500,
+disableOnInteraction: false,
+  },
+  centerSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
